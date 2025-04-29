96c1aa366c1360bb8724e590c23e62b10812ee70
Alex Dias <alxyoq@gmail.com>
2025-04-29 18:36:44 -0400

    temp

--- src/shared/bookingURL.ts
+++
@@ -1,4 +0,0 @@
-const bookingURL =
-  "https://book.squareup.com/appointments/4n5ksms38sadup/location/HE59YBWJWJYDC/services?buttonTextColor=ffffff&color=000000&locale=en&referrer=so";
-
-export default bookingURL;
